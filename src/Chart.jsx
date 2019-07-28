5a8086060717bacc4bcdd485c88bf383bbdd3f22
Jack Rhodes <jack.rhodes@ibm.com>
2019-07-28 10:25:37 +0100

    Set x type and give labels

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { Line, LineChart, XAxis, YAxis } from "recharts";
+import { Line, LineChart, XAxis, YAxis, Label } from "recharts";
 
 const Chart = ({ data }) => {
   return (
-    <LineChart width={600} height={400} data={data}>
-      <XAxis />
-      <YAxis />
+    <LineChart
+      width={600}
+      height={400}
+      data={data}
+      margin={{ top: 30, right: 30, left: 30, bottom: 30 }}
+    >
+      <XAxis type="number" dataKey="time" domain={["dataMin", "dataMax"]}>
+        <Label
+          value={"Time"}
+          position="bottom"
+          style={{ textAnchor: "middle" }}
+        />
+      </XAxis>
+      <YAxis>
+        <Label
+          value={"Temperature"}
+          position="left"
+          angle={-90}
+          style={{ textAnchor: "middle" }}
+        />
+      </YAxis>
       <Line dataKey="temperature" />
     </LineChart>
   );
